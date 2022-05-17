c99cc5e41f8ced376a6bd75756e778a46fe111b3
Ren-Langlois <101406518+Ren-Langlois@users.noreply.github.com>
2022-05-17 12:09:13 -0300

    17/5

---
+++ Assets/PlayerControler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public float movementSpeed;
+    public float movementASpeed;
+    public float movementWSpeed;
+    public float movementSSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(movementSpeed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= new Vector3(movementASpeed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, movementWSpeed, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= new Vector3(0, movementSSpeed, 0);
+        }
+    }
+}
